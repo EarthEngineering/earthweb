49f77ed58f2d0116cbc635ebc5a1974d19402305
Gabriel Cardona <cgcardona@gmail.com>
2019-12-05 09:44:52 -0800

    prefix

--- src/utils/address.js
+++ src/utils/address.js
@@ -1,7 +1,7 @@
 export const ADDRESS_SIZE = 34;
-export const ADDRESS_PREFIX = "41";
-export const ADDRESS_PREFIX_BYTE = 0x41;
-export const ADDRESS_PREFIX_REGEX = /^(41)/;
-// export const ADDRESS_PREFIX = "21";
-// export const ADDRESS_PREFIX_BYTE = 0x21;
-// export const ADDRESS_PREFIX_REGEX = /^(21)/;
+// export const ADDRESS_PREFIX = "41";
+// export const ADDRESS_PREFIX_BYTE = 0x41;
+// export const ADDRESS_PREFIX_REGEX = /^(41)/;
+export const ADDRESS_PREFIX = "19";
+export const ADDRESS_PREFIX_BYTE = 0x19;
+export const ADDRESS_PREFIX_REGEX = /^(19)/;
