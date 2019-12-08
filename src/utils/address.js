531e46b1e2a74b4b43780e39725b06e43484967b
Gabriel Cardona <cgcardona@gmail.com>
2019-12-08 07:51:23 -0800

    21 for prefix byte.

--- src/utils/address.js
+++ src/utils/address.js
@@ -1,8 +1,8 @@
 export const ADDRESS_SIZE = 34;
-export const ADDRESS_PREFIX = "41";
-export const ADDRESS_PREFIX_BYTE = 0x41;
-export const ADDRESS_PREFIX_REGEX = /^(41)/;
+// export const ADDRESS_PREFIX = "41";
+// export const ADDRESS_PREFIX_BYTE = 0x41;
+// export const ADDRESS_PREFIX_REGEX = /^(41)/;
 // TODO get addresses to start with `E`
-// export const ADDRESS_PREFIX = "21";
-// export const ADDRESS_PREFIX_BYTE = 0x21;
-// export const ADDRESS_PREFIX_REGEX = /^(21)/;
+export const ADDRESS_PREFIX = "21";
+export const ADDRESS_PREFIX_BYTE = 0x21;
+export const ADDRESS_PREFIX_REGEX = /^(21)/;
