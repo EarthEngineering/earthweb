6d96d1b9dc99b7e474cad7eea49a448413a9e1d6
Gabriel Cardona <cgcardona@gmail.com>
2019-12-12 16:48:37 -0800

    README.

---
+++ examples/README.md
@@ -0,0 +1,30 @@
+# Examples
+
+Here are examples which cover all of earthweb's functionality. They're broken up in to 4 main sections
+
+-   earth
+-   earthweb
+-   tokens
+-   transactionBuilder
+
+earth can be broken in to further sections.
+
+-   earth
+    -   earth and accounts
+    -   dex
+    -   multisignature apis
+    -   proposals
+    -   query network
+    -   smart contracts
+    -   token
+    -   transactions
+    -   voting and super representatives
+
+## Setup
+
+First run `npm install` from this diretory to install `earthweb` locally. Next `cd` in to the directory fo the test which you intend to run and run it via the `node` comand. For example to call the `listTokens.js` example
+
+```
+cd examples/tokens
+node listTokens.js
+```
---
+++ examples/transactionBuilder/sendToken.js
@@ -0,0 +1,27 @@
+// create an async main function to leverage await below
+async function main() {
+    // require earthweb
+    let EarthWeb = require("../../dist/EarthWeb.node.js");
+    let fullNodePrivateKey =
+        "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0";
+
+    // instantiate EarthWeb
+    const earthweb = await new EarthWeb({
+        fullHost: "http://127.0.0.1:9090",
+        solidityHost: "http://127.0.0.1:9090",
+        eventServer: "http://127.0.0.1:9090",
+        privateKey: fullNodePrivateKey
+    });
+
+    let sendTrx = await earthweb.transactionBuilder.sendToken(
+        "TVyrnRU44hUecS6Ji2AQwBQ9oZ76Xenmqz",
+        11,
+        "1000001",
+        "TVyrnRU44hUecS6Ji2AQwBQ9oZ76Xenmqz"
+    );
+
+    console.log(sendTrx);
+}
+
+// make sure to call main() to start the dapp
+main();
