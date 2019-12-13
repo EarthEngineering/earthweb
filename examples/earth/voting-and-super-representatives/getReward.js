8947fd548956c8fa83197255b97a47d5e9e0d1ae
Gabriel Cardona <cgcardona@gmail.com>
2019-12-12 16:30:51 -0800

    Smart contracts and voting and super representatives.

---
+++ examples/earth/voting-and-super-representatives/getReward.js
@@ -0,0 +1,24 @@
+// create an async main function to leverage await below
+async function main() {
+    // require earthweb
+    let EarthWeb = require("../../../dist/EarthWeb.node.js");
+    let fullNodePrivateKey =
+        "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0";
+
+    // instantiate EarthWeb
+    const earthweb = await new EarthWeb({
+        fullHost: "http://127.0.0.1:9090",
+        solidityHost: "http://127.0.0.1:9090",
+        eventServer: "http://127.0.0.1:9090",
+        privateKey: fullNodePrivateKey
+    });
+
+    let getReward = await earthweb.earth.getReward(
+        "TByyPTZ1BU91kjD2rPYYk27JnE8jNVmcAg"
+    );
+
+    console.log(getReward);
+}
+
+// make sure to call main() to start the dapp
+main();
