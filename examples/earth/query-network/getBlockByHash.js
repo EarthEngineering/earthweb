325869269594ebaccc7b2c0e13b6a5f76384097f
Gabriel Cardona <cgcardona@gmail.com>
2019-12-12 16:23:22 -0800

    Proposals and query network.

---
+++ examples/earth/query-network/getBlockByHash.js
@@ -0,0 +1,24 @@
+// create an async main function to leverage await below
+async function main() {
+    // require earthweb
+    let EarthWeb = require("../../../dist/EarthWeb.node.js");
+    let fullNodePrivateKey =
+        "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0";
+
+    // instantiate EarthWeb
+    const earthweb = await new EarthWeb({
+        fullHost: "http://127.0.0.1:9090",
+        solidityHost: "http://127.0.0.1:9090",
+        eventServer: "http://127.0.0.1:9090",
+        privateKey: fullNodePrivateKey
+    });
+
+    let getBlock = await earthweb.earth.getBlockByHash(
+        "00000000000000013430f4ceff4a799ab4ea0c2a9f684c0526ce6d3068690670"
+    );
+
+    console.log(getBlock);
+}
+
+// make sure to call main() to start the dapp
+main();
